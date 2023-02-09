26ac7d542f48d0b739eb818b9c3dc40d9a693b68
Vitor-devs <vitorcoelho2004@outlook.com>
2023-02-09 07:47:02 -0300

    ambiente virtual

---
+++ AmbVirtual/ambvirtual.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pymysql
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+
+# python -m venv nomedoambiente
+# ele vai para os interpretadores
+# pip freeze > requirements.txt
+# pip install -r ./requirements.txt
+# pip install -r .\requirements.txt
